320d5827cef9fc8586285caca43c210c06822b69
Mustafa Husain <mustafahusain784@gmail.com>
2025-09-09 08:17:36 +0530

    Delete database.py

--- database.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-def get_connection():
-    return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="hello",
-        database="Shop_Management"
-    )
